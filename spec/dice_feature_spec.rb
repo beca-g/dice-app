744ed0d2c547acc4fa247ccbd5c853d4a433c5bb
Beca-g <rebekahgalliano@gmail.com>
2021-02-08 18:18:09 +0000

    A dice roll randomly  generates a number between 1 and 6
    As a board game player,
    So that I know how many steps I should move
    Rolling a dice should give me a number between 1 and 6
    
    As a dice app developer,
    So that I give players a good game experience
    If it is not already random, I want the dice roll to be randomly picked

--- spec/dice_feature_spec.rb
+++ spec/dice_feature_spec.rb
@@ -8,4 +8,19 @@ describe "user stories" do
     dice = Dice.new
     expect(dice).to respond_to(:roll)
   end
+
+  # As a board game player,
+  # So that I know how many steps I should move
+  # Rolling a dice should give me a number between 1 and 6
+
+  # As a dice app developer,
+  # So that I give players a good game experience
+  # If it is not already random, I want the dice roll to be randomly picked
+
+  it "a dice roll generates a random number between 1-6" do
+    dice = Dice.new
+    dice_roll = dice.roll
+    expect(dice_roll).to be_between 1, 6
+  end
+
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -7,5 +7,10 @@ describe Dice do
       expect(dice).to receive(:roll)
       dice.roll
     end
+
+    it "a dice roll generates a number between 1 and 6" do
+      dice_roll = dice.roll
+      expect(dice_roll).to be_between 1, 6
+    end
   end
 end
